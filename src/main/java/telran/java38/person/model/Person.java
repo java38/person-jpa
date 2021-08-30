8d0d462595954f285b53f730171705bb3d506668
EddFish <edwardlfish@gmail.com>
2021-08-30 21:10:24 +0300

    Complete

--- src/main/java/telran/java38/person/model/Person.java
+++ src/main/java/telran/java38/person/model/Person.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,12 +21,12 @@ import lombok.Setter;
 @EqualsAndHashCode(of = {"id"})
 @Entity(name = "Adon")
 @Table(name = "person")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Person {
 	
 	@Id
 	Integer id;
 	String name;
 	LocalDate birthDate;
-	//@EmbeddedId
 	Address address;
 }
